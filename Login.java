383c9695b61dad025125913e9dbd1c91a2841545
Nigel Goes <nigelgoes@Nigels-MacBook-Pro-3.local>
2025-08-04 10:59:44 +0100

    Added username feature to detect and block offensive words

--- Login.java
+++ Login.java
@@ -12,72 +12,108 @@ public class Login{
         
         System.out.println(" Thank you for entering your name.");
         System.out.println();
-/// Email ///
-        String email = "";
-        while(true){
-            System.out.println("Please enter your email Address:");
-            email = input.nextLine();
 
-            if(email.contains("@") && email.endsWith("@gmail.com")){
-                System.out.println("email address is correct");
-                System.out.println();
-                break;
-            }else{
-                System.out.println("email address does not meet the requirements. ");
-                System.out.println("Please re-entetr the email again: ");
+
+
+// /// Email ///
+//         String email = "";
+//         while(true){
+//             System.out.println("Please enter your email Address:");
+//             email = input.nextLine();
+
+//             if(email.contains("@") && email.endsWith("@gmail.com")){
+//                 System.out.println("email address is correct");
+//                 System.out.println();
+//                 break;
+//             }else{
+//                 System.out.println("email address does not meet the requirements. ");
+//                 System.out.println();
+//                 System.out.println("Please re-entetr the email again. ");
+//                 System.out.println();
                 
-            }
-        }
+//             }
+//         }
         
-        // User ///
+        // Username selection///
 
+        String[] prohibihtedWords = {"hoe", "idiot", "slut", "bitch", "asshole",};
         String user = "";
 
         while(true){
-            System.out.print("Please enter your preffered username:");
+            System.out.println("Please enter your preferred username:");
             user = input.nextLine();
 
-            if(user.equals("hoe")){
-                System.out.println("Please re enter your username as its offensive");
-    
-            }else{
-                System.out.println("username meets the needs of the company");
-                break;
-    
+            boolean isOffensive = false; 
+
+            for(int i = 0; i <prohibihtedWords.length; i++){
+                if(user.toLowerCase().contains(prohibihtedWords[i].toLowerCase())){
+                    isOffensive = true;
+                    break;
+                }
             }
+            if(isOffensive){
+                System.out.println("PLEASE RE- ENTER YOUR USERNAME AS ITS OFFENSIVE TO OUR GUIDELINES.");
+                System.out.println();
+            }else{
+                System.out.println("Usename meets the needs of the company");
+                System.out.println();
+                break;
 
-        }      
+            }
+        }
 
-        System.out.println(" Hello " + user);
-        System.out.println(" Welcome " + name + "  " + user);
+
+
+
+
+        // String user = "";
+
+        // while(true){
+        //     System.out.print("Please enter your preffered username:");
+        //     user = input.nextLine();
+
+        //     if(user.equals("hoe")){
+        //         System.out.println("Please re enter your username as its offensive");
+    
+        //     }else{
+        //         System.out.println("username meets the needs of the company");
+        //         break;
+    
+        //     }
+
+        // }      
+
+        // System.out.println(" Hello " + user);
+        // System.out.println(" Welcome " + name + "  " + user);
 
 
         ///Password///
 
-        while(true){
-            System.out.print(" Now create your password:");
-            String pass = input.nextLine();
+        // while(true){
+        //     System.out.print(" Now create your password:");
+        //     String pass = input.nextLine();
     
-            if(pass.length()<=6){
-                System.out.println("Please use more than 6 characters");
+        //     if(pass.length()<=6){
+        //         System.out.println("Please use more than 6 characters");
     
-            }else{
-                System.out.print(" Confirm your password: ");
-                String checkPass = input.nextLine();
+        //     }else{
+        //         System.out.print(" Confirm your password: ");
+        //         String checkPass = input.nextLine();
 
-                if(pass.equals(checkPass)){
-                    System.out.println("u have entered the password correctly");
-                    break;
+        //         if(pass.equals(checkPass)){
+        //             System.out.println("You have entered the password correctly");
+        //             break;
                 
-                } else{
-                    System.out.println(" Paswords do not match");
-                }
-            }
+        //         } else{
+        //             System.out.println(" Paswords do not match");
+        //         }
+        //     }
     
-        }
+        // }
 
 
 } 
+
 }
 
  
